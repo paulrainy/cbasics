fcbde56731c6b559038b4c1d5776374bbf472b25
paulrainy <science0instant0issue@gmail.com>
2023-07-19 10:26:51 +0300

    checking the task

--- task4/main.c
+++ task4/main.c
@@ -1,6 +1,31 @@
+//square of zero's in int matrix
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void matrix_out(int N, int M, int matrix[N][M]){
+    for (int i = 0; i < N; i++){
+        for (int j = 0; j < M; j++){
+            printf("%d", matrix[i][j]);
+            printf(" ");
+        }
+        printf("\n");
+    }
+}
 
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+    srand(time(NULL));
+    int N, M;
+    N = 1 + rand() % 11;
+    M = 1 + rand() % 11;
+    int matrix[N][M];
+    for (int i = 0; i < N; i++){
+        for (int j = 0; j < M; j++){
+            matrix[i][j] = rand() % 9;
+        }
+    }
+
+    matrix_out(N, M, matrix);
+
+
 }
---
+++ task5/main.c
@@ -0,0 +1,21 @@
+//main diag not included
+//need to find max num in upper part of matrix
+//then check existence of this max num in lower part
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void matix_out_row(){
+
+}
+
+void matrix_out_col(){
+
+}
+
+int main() {
+
+
+    return 0;
+}
